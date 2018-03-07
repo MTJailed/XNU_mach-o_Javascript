a2ed8f2091d93d343dd0958deb61cda4f0b913e9
MTJailed <marketing@jailed.ml>
2018-03-07 22:52:58 +0100

    Optimized performance

--- endianreader.jsh.js
+++ endianreader.jsh.js
@@ -11,14 +11,4 @@ var EndianReader = function EndianReader()
 		return parseInt('0x'+result.join(''));
 	}
 };
-EndianReader.swap32 = function swap32(val)
-{
-	var string = val.toString(16);
-		var result = [];
-		let len = string.length - 2;
-		while(len >= 0) {
-			result.push(string.substr(len, 2));
-			len -= 2;
-		}
-		return parseInt('0x'+result.join(''));
-};
+EndianReader.swap32 = new EndianReader().swap32;
